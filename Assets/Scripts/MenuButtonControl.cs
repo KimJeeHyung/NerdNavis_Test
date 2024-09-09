9e76a073cdffd8a5bb4b3b4a87dbe79f726b6606
KimJeeHyung <wlgud1002@gmail.com>
2024-09-10 06:48:01 +0900

    메뉴 전환 기능 추가
    화면 아래 두 개의 버튼을 클릭해 자원 메뉴와 뽑기 메뉴를 전환하는 기능 구현
    또한 현재 메뉴에 따라 버튼 색깔이 달라지는 기능 구현

---
+++ Assets/Scripts/MenuButtonControl.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq.Expressions;
+
+public class MenuButtonControl : MonoBehaviour
+{
+    [SerializeField] private GameObject ResourcesMenu;
+    [SerializeField] private GameObject GachaMenu;
+
+    [SerializeField]
+    private Button ResourcesMenuButton;
+    private Image ResourcesMenuBackground;
+    private TextMeshProUGUI ResourcesMenuText;
+
+    [SerializeField]
+    private Button GachaMenuButton;
+    private Image GachaMenuBackground;
+    private TextMeshProUGUI GachaMenuText;
+
+    private Color SelectedColor;
+
+    [SerializeField] private bool b_ResourcesMenuSelected = false;
+    [SerializeField] private bool b_GachaMenuSelected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectedColor = new Color32(0, 83, 166, 255);
+
+        ResourcesMenuBackground = ResourcesMenuButton.GetComponent<Image>();
+        ResourcesMenuText = ResourcesMenuButton.GetComponentInChildren<TextMeshProUGUI>();
+
+        GachaMenuBackground = GachaMenuButton.GetComponent<Image>();
+        GachaMenuText = GachaMenuButton.GetComponentInChildren<TextMeshProUGUI>();
+
+        ResourcesMenuBackground.color = SelectedColor;
+        ResourcesMenuText.color = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickResourcesMenuButton()
+    {
+        ResourcesMenu.SetActive(true);
+        GachaMenu.SetActive(false);
+
+        ResourcesMenuBackground.color = SelectedColor;
+        ResourcesMenuText.color = Color.white;
+
+        GachaMenuBackground.color = Color.white;
+        GachaMenuText.color = Color.black;
+    }
+
+    public void OnClickGachaMenuButton()
+    {
+        GachaMenu.SetActive(true);
+        ResourcesMenu.SetActive(false);
+
+        GachaMenuBackground.color = SelectedColor;
+        GachaMenuText.color = Color.white;
+
+        ResourcesMenuBackground.color = Color.white;
+        ResourcesMenuText.color = Color.black;
+    }
+}
